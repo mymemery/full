5bc11f4e92e32378a4369824837eaf76886a0b10
Edmond Tam <edmondtam2@gmail.com>
2024-02-25 16:07:19 +0000

    Multi-link form working

--- src/components/add-memeries.tsx
+++ src/components/add-memeries.tsx
@@ -1,22 +1,32 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, useForm } from "react-hook-form";
+import { XCircle } from "lucide-react";
+import { useFieldArray, useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
-import { logger } from "@/lib/logger";
-
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "./ui/dialog";
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { logger } from "@/lib/logger";
 
-const formSchema = z.object({
-  url: z.string().url(),
+const FormSchema = z.object({
+  // urls: z.string().url().array(),
+  urls: z.array(z.object({ url: z.string().url() })),
 });
 
 export default function AddMemeries() {
@@ -32,7 +42,7 @@ export default function AddMemeries() {
           <DialogHeader>
             <DialogTitle>Add Memeries</DialogTitle>
           </DialogHeader>
-          {/* <AddMemeriesForm /> */}
+          <AddMemeriesForm />
         </DialogContent>
       </Dialog>
     </div>
@@ -40,40 +50,80 @@ export default function AddMemeries() {
 }
 
 export function AddMemeriesForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
     defaultValues: {
-      url: "",
+      urls: [{ url: "" }],
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "urls",
+  });
+
+  const onSubmit = (values: z.infer<typeof FormSchema>) => {
     logger.info("Adding memery: ", values);
   };
+
   return (
     <>
       <Form {...form}>
-        <form id="addMemeries" onSubmit={form.handleSubmit(onSubmit)}>
-          <div className="flex w-full max-w-sm items-center space-x-2">
-            {/* <FormField
-              control={form.control}
-              name="url"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input placeholder="https://your-blog.com/123" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            /> */}
-            hi
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className={"h-80 space-y-2 overflow-auto"}
+        >
+          {fields.map((field, index) => {
+            return (
+              <FormField
+                control={form.control}
+                key={field.id}
+                name={`urls.${index}`}
+                render={() => {
+                  return (
+                    <FormItem>
+                      <FormLabel>Link {index + 1}</FormLabel>
+                      <div className="relative">
+                        <Input
+                          placeholder="https://mymemery.co.uk"
+                          {...form.register(`urls.${index}.url`)}
+                        />
+                        {fields.length > 1 && (
+                          <Button
+                            type="button"
+                            onClick={() => remove(index)}
+                            className="absolute right-2 top-1/2 -translate-y-1/2 text-slate-400"
+                            variant="ghost"
+                          >
+                            <XCircle className="" />
+                          </Button>
+                        )}
+                      </div>
+                      <FormControl />
+
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+            );
+          })}
+          <div className="flex items-center justify-between">
+            <Button
+              type="button"
+              variant="secondary"
+              onClick={() => {
+                append({ url: "" });
+              }}
+            >
+              +
+            </Button>
+            <Button type="submit">
+              Scrape {fields.length} link{fields.length > 1 ? "s" : ""}
+            </Button>
           </div>
         </form>
       </Form>
-      <Button form="addMemeries" variant="outline">
-        +
-      </Button>
     </>
   );
 }
